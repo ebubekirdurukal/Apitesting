20b9fa5d461efe2f10962ab7e3764e9ed2d02258
Ebubekir Durukal <ebubekir.durukal@airties.com>
2023-03-05 13:58:34 +0300

    new annotations added

--- src/test/java/org/example/ExampleTest.java
+++ src/test/java/org/example/ExampleTest.java
@@ -2,19 +2,44 @@ package org.example;
 
 import io.restassured.response.ResponseBody;
 import org.hamcrest.Matchers;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 
 import static io.restassured.RestAssured.given;
 
 public class ExampleTest {
 
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Using @BeforeClass , executed before all test cases ");
+    }
+
+    @Before
+    public void m2() {
+        System.out.println("Using @Before annotations ,executed before each test cases ");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("Using @AfterClass ,executed after all test cases");
+    }
+
+    @After
+    public void after() {
+        System.out.println("Using @After ,executed after each test cases");
+    }
+
+    @Ignore
+    public void ignored() {
+        System.out.println("Using @Ignore , this execution is ignored");
+    }
+
     @Test
+    @Ignore
     public void firstTest() {
         Assert.assertNotEquals("Two values are equal! ", 4, 2 + 2);
     }
 
-    @Test
+    @Test(timeout = 100)
     public void statusCodeTest() {
         given()
                 .when()
